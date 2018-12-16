b793b1d353c88a9e8d83dfe82e85e7cb6b47fb64
J <j.e.aten@gmail.com>
2018-12-15 18:13:46 -0600

    green run_test for importing pandas from dynamically added import path

--- run_test.go
+++ run_test.go
@@ -1,25 +1,87 @@
-package py_test
+package py
 
 import (
+	"fmt"
+	"os"
 	"testing"
-
-	"github.com/glycerine/gopy"
 )
 
+func TestAddingImportPathAndRun(t *testing.T) {
+
+	goPath := os.Getenv("GOPATH")
+	pathAdd := goPath + "/src/github.com/glycerine/gp/install/env3/lib/python3.7/site-packages"
+
+	Initialize()
+	defer Finalize()
+	main, err := NewDict()
+	if err != nil {
+		t.Fatal(err)
+	}
+	g, err := GetBuiltins()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// g = '&py.Dict{AbstractObject:py.AbstractObject{}, o:(*py._Ctype_struct___154)(0x5281900)}'
+	vv("g = '%#v'", g)
+
+	err = main.SetItemString("__builtins__", g)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = RunString("a = 'hello world!'", FileInput, main, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	a, err := main.GetItemString("a")
+	if err != nil {
+		t.Fatal(err)
+	}
+	b, ok := a.(*Unicode)
+	if !ok || b.String() != "hello world!" {
+		t.Error(b, err)
+	}
+
+	_, err = RunString(fmt.Sprintf("import sys; sys.path.insert(0, '%s'); sp=sys.path;", pathAdd), FileInput, main, nil)
+	panicOn(err)
+
+	sysPath, err := main.GetItemString("sp")
+	panicOn(err)
+
+	vv("sysPath = '%s'", sysPath)
+
+	_, err = RunString("import pandas;", FileInput, main, nil)
+	panicOn(err)
+
+}
+
 func TestRunString(t *testing.T) {
-	py.Initialize()
-	defer py.Finalize()
-	if main, err := py.NewDict(); err != nil {
+
+	Initialize()
+	defer Finalize()
+	main, err := NewDict()
+	if err != nil {
 		t.Fatal(err)
-	} else if g, err := py.GetBuiltins(); err != nil {
+	}
+	g, err := GetBuiltins()
+	if err != nil {
 		t.Fatal(err)
-	} else if err := main.SetItemString("__builtins__", g); err != nil {
+	}
+
+	err = main.SetItemString("__builtins__", g)
+	if err != nil {
 		t.Fatal(err)
-	} else if _, err := py.RunString("a = 'hello world!'", py.FileInput, main, nil); err != nil {
+	}
+	_, err = RunString("a = 'hello world!'", FileInput, main, nil)
+	if err != nil {
 		t.Fatal(err)
-	} else if a, err := main.GetItemString("a"); err != nil {
+	}
+	a, err := main.GetItemString("a")
+	if err != nil {
 		t.Fatal(err)
-	} else if b, ok := a.(*py.Unicode); !ok || b.String() != "hello world!" {
+	}
+	b, ok := a.(*Unicode)
+	if !ok || b.String() != "hello world!" {
 		t.Error(b, err)
 	}
 }
