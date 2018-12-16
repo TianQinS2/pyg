22de6453154468bd0fc01f8803cfff9c52e1080c
J <j.e.aten@gmail.com>
2018-12-15 23:45:44 -0600

    update doc

--- doc.go
+++ doc.go
@@ -3,25 +3,16 @@
 // license that can be found in the LICENSE file.
 
 /*
-Package py (github.com/glycerine/gopy) provides access to the CPython C API.  This
-package presents an idomatic Go interface to the CPython C API described at
-http://docs.python.org/c-api/index.html
+`pyg` is forked and derived from https://github.com/qur/gopy; it has been adapted to target python 3.7.1.
 
-Instead of simply exposing the C API as-is, this package uses interfaces,
-embedding, type assertions and methods to try and present the functionality of
-the Python API in a manner that feels more natural in Go.
-
-Embedding Python
+This package presents an idomatic Go interface to the CPython C API described at
+http://docs.python.org/3/c-api/index.html
 
 Embedding Python is fully supported, with the ability to initialise the
 interpreter, enable threading support, manipulate the GIL and call Python API
 functions to manipulate Python objects.
 
-In addition to providing the ability to use the API to call into Python calling
-from Python back into Go is also supported.  New types can be implemented in Go
-and exposed into Python.
-
-The package targets Python 3.7.1 at present.
+Installation is a simple matter of re-pointing the `python-3.7m.pc` symlink to point to the pkg-config .pc file in your python installation.
 
 */
 package pyg
